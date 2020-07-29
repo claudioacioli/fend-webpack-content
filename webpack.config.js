35f03967c010b34ab48296eae7abfd6d3d4fe25e
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-07-28 21:42:25 -0300

    chore: Configure loader babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,4 +2,19 @@ const path = require("path")
 const webpack = require("webpack")
 module.exports = {
   entry: "./src/client/index.js",
+  module: {
+    rules: [
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader"
+      },
+      {
+        test: '/\.js$/',
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  }
 }
